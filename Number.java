d328fb1a9e630504c180cfc892d6cd56ad8b422c
SRK03 <rk324096@gmail.com>
2017-07-06 11:36:40 +0530

    Create Number.java

---
+++ Number.java
@@ -0,0 +1,15 @@
+import java.util.*
+class Number
+{
+public static void main(String args[])
+{
+Scanner scan=new Scanner(System.in)
+int n=scan.nextInt();
+if(n==0)
+System.out.print("Zero");
+elseif(n>0)
+System.out.print("Possitive");
+else
+System.out.print("Negative);
+}
+}
