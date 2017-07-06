6900be5f1f48643d194980b114d27aca91fee72f
SRK03 <rk324096@gmail.com>
2017-07-06 12:00:24 +0530

    Create Vowel.java

---
+++ Vowel.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Vowel
+{
+public static void main(String args[])
+{
+Scanner scan=new Scanner(System.in);
+Char ch=scan.nextChar();
+if(ch=='a' || ch=='A' || ch=='e' || ch=='E' ch=='i' || ch=='I' ch=='o' || ch=='O' ch=='u' || ch=='U')
+System.out.print("vowel");
+else
+System.out.print("consodant");
+}
+}
