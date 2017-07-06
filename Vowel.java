8c22286a8f42a295a144981dc3d4419939c0122a
SRK03 <rk324096@gmail.com>
2017-07-06 12:15:16 +0530

    Update Vowel.java

--- Vowel.java
+++ Vowel.java
@@ -4,7 +4,7 @@ class Vowel
 public static void main(String args[])
 {
 Scanner scan=new Scanner(System.in);
-Char ch=scan.nextChar();
+Char ch=scan.next().charAt(0);
 if(ch=='a' || ch=='A' || ch=='e' || ch=='E' ch=='i' || ch=='I' ch=='o' || ch=='O' ch=='u' || ch=='U')
 System.out.print("vowel");
 else
