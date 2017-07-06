a68fdb5e47c639540b4422ecfc151dff5009106f
SRK03 <rk324096@gmail.com>
2017-07-06 11:47:00 +0530

    Create Modulo.java

---
+++ Modulo.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Modulo
+{
+  public static void main(String args[])
+  {
+    Scanner scan=new Scanner(System.in);
+    int n=scan.nextInt();
+    if(n%2==0)
+    System.out.print("even");
+    else
+    System.out.print("odd");
+  }
+}
